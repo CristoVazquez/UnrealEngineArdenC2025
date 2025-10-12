a4e9c688a3b148a53f9437518c0fadbe2752c84b
Cristo Oliver <144202664+CristoVazquez@users.noreply.github.com>
2025-10-12 16:32:13 -0600

    clase1
    parte final

---
+++ UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "D:/Oliver/GitHub/UnrealEngineArdenC2025/UnrealEngineArdenC2025/UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "D:/Oliver/GitHub/UnrealEngineArdenC2025/UnrealEngineArdenC2025/UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
